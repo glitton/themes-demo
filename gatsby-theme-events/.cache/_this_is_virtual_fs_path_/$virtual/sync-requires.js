4b49c6c385624cd8dec7c293da4f2e67a7cc7ed7
Generosa Litton <glcodeworks@gmail.com>
2021-05-20 15:28:54 -0500

    first commit

---
+++ gatsby-theme-events/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,9 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/glitton/Demos/themes-demo/gatsby-theme-events/.cache/dev-404-page.js"))
+}
+
