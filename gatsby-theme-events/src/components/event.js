48470e58af0603fcc789840ab5bfd227d9252396
Generosa Litton <glcodeworks@gmail.com>
2021-05-28 14:14:01 -0500

    fiddled with date

--- gatsby-theme-events/src/components/event.js
+++ gatsby-theme-events/src/components/event.js
@@ -1,12 +1,40 @@
 import React from "react";
 
+const getDate = (date, { day = true, month = true, year = true } = {}) =>
+  date.toLocaleString("en-US", {
+    day: day ? "numeric" : undefined,
+    month: month ? "long" : undefined,
+    year: year ? "numeric" : undefined,
+  });
+
+const EventDate = ({ startDate, endDate }) => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const isOneDay = start.toDateString() === end.toDateString();
+
+  console.log(start.getMonth(), end.getMonth());
+
+  return (
+    <>
+      <time dateTime={start.toISOString()}>
+        {/* only show the year if isOneDay is true */}
+        {getDate(start, { year: isOneDay })}
+      </time>
+      {!isOneDay && (
+        <>
+          {""} - <time dateTime={end.toISOString()}>{getDate(end)}</time>
+        </>
+      )}
+    </>
+  );
+};
 const Event = ({ name, location, url, startDate, endDate }) => (
   <div>
     <h1>
       {name} ({location})
     </h1>
     <p>
-      {startDate}-{endDate}
+      <EventDate startDate={startDate} endDate={endDate} />
     </p>
     <p>
       Website: <a href={url}>{url}</a>
